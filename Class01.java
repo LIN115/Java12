0d36e8fe206244a4b6a7978fc70239daae71b666
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-11-26 21:12:21 +0800

    Add files via upload

---
+++ Class01.java
@@ -0,0 +1,21 @@
+package Java12;
+
+class CRectangle {
+	int width;
+	int height;
+}
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CRectangle rect1;
+		rect1 = new CRectangle();
+		rect1.width = 10;
+		rect1.height = 5;
+		System.out.println("width=" + rect1.width);
+		System.out.println("height=" + rect1.height);
+
+	}
+
+}
